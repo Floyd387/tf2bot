3ff730b8e4e2c60525b12db24f91ce6fd45aaa8b
Floyd387 <51698414+Floyd387@users.noreply.github.com>
2019-10-26 00:21:30 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+// Basic login info
+
+    botName: '',
+    accountName: '',
+    password: '',
+    sharedSecret: '',
+    identitySecret: '',
+    botAdminAccount: '',
+    mySteamAPIkey: ''
+}
