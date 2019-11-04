2a411d29c7db71374f71d3a2e17c1f418b0512bf
Floyd387 <51698414+Floyd387@users.noreply.github.com>
2019-11-04 10:34:06 +0100

    Add files via upload

---
+++ config (copy).js
@@ -0,0 +1,16 @@
+module.exports = {
+
+// Basic login info
+
+    botName: '',
+    botSteamID: '',
+    accountName: '',
+    bpTfToken: '',
+    bpTfApiKey: '',
+    password: '',
+    sharedSecret: '',
+    identitySecret: '',
+    botAdminAccount: '',
+    mySteamAPIkey: '',
+    keyToMetalConversionRate: '51.33'
+}
